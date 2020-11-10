32e35d89f7dec871594d0d9bdbdfefb2beba5e83
rhnarora4 <73931663+rhnarora4@users.noreply.github.com>
2020-11-10 20:47:37 +0530

    A seperate code is written to minuplate Bulb with help[ of GSM(Controller used: 8051)

---
+++ Bulb_Manipulation/common.h
@@ -0,0 +1,11 @@
+#ifndef __COMMON__
+#define __COMMON_ 1
+
+#include<reg51.h>
+#include<string.h>
+#include<stdlib.h>
+#include"datatypes.h"
+#include"register_names.h"
+#include"global_data.h"
+
+#endif
