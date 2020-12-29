500f2bf9d5b65223a8d97f5553ccb1edb468913a
Rohan Arora <rhnarora4@gmail.com>
2020-12-29 22:30:36 +0530

    Removed unnecessary code
    Removed unnecessary function declaration.

--- GGVTS/code/response.h
+++ GGVTS/code/response.h
@@ -1,6 +1,5 @@
 #ifndef __RESPONSE__
 #define __RESPONSE__
   extern UINT32 check_response(void);
-	extern void response_to_user(const INT8 *);
-	void collecting_location_info(void);
+  extern void response_to_user(const INT8 *);
 #endif
