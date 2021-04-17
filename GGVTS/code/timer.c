a0593a89fa4f0497ecae0eda8c8c875c79c25cdb
Rohan Arora <creativegroup8592@gmail.com>
2021-04-17 21:03:48 +0530

    Updated Headers
    Updated below things:
    1. Function headers
    2. Comments to code
    3. Indentation

--- GGVTS/code/timer.c
+++ GGVTS/code/timer.c
@@ -1,52 +1,83 @@
+/***************************************************************************
+*  File Name: timer.c                                                      *
+*--------------------------------------------------------------------------*
+*  Description: This file contains all the function definitions used for   *
+*               timers.                                                    *
+*                                                                          *
+*--------------------------------------------------------------------------*
+*  Comments:	                                                           *
+*                                                                          *
+***************************************************************************/
+
+/***************************************************************************
+*  Below are the header files required to build this file                  *
+***************************************************************************/
 #include "common.h"
 #undef DEBUG_START
+
+/***************************************************************************
+*  Funtion Name: timer_isr                                                 *
+***************************************************************************/
 __irq void timer_isr(void)
 {
-	  T1IR = 0x01;
-	  #ifdef DEBUG_START
-        debug("Timer Interrupt Start\n");
-    #endif
-    SEND_LOCATION = ON;
-		  #ifdef DEBUG_START
-        debug("Timer Interrupt End\n");
-    #endif
-		VICVectAddr = 0x00;
+    T1IR = 0x01;                        // Clear timer interrupt
+	#ifdef DEBUG_START                  //Debug Purpose
+        debug("Timer Interrupt Start\n"); //Debug purpose
+    #endif                                //Debug purpose
+    SEND_LOCATION = ON;                   //Global variable to enable SEND_FREQ functionality.
+    #ifdef DEBUG_START                    //Debug purpose
+        debug("Timer Interrupt End\n");   //Debug purpose
+    #endif                                //Debug purpose
+	VICVectAddr = 0x00;                   //This line tells processor that interrupt ends here.
 }
+
+/***************************************************************************
+*  Funtion Name: set_location_frequency                                    *
+***************************************************************************/
 void set_location_frequency(UINT32 freq)
 {
-	T1PR = 11999 * freq * 60; 			 //Value to make delay of 2 minute
-	#ifdef DEBUG_START
-      debug("Timer Start");
-  #endif
-	T1TCR = 0x01;        //Start Timer
+    T1PR = 11999 * freq * 60; 	//To make timer run for desired time, freq can have values 1,3,5,7,10
+    #ifdef DEBUG_START          //Debug purpose
+        debug("Timer Start");   //Debug purpose
+    #endif                      //Debug purpose
+	T1TCR = 0x01;               //Start Timer
 	return;
 }
 
+/***************************************************************************
+*  Funtion Name: delay                                                     *
+***************************************************************************/
 void delay(float seconds)
 {
-		T0PR = (UINT32)(11999 * seconds); 			 //Value to make delay of 1ms
-	  T0TCR = 0x01;        //Start Timer
-		while(T0TCR);        //Wait until timer reset (1 sec delay generated)
-		T0TCR = 0x00;        //Stop Timer
+    T0PR = (UINT32)(11999 * seconds); 	   //Value to make delay of desired time.
+	T0TCR = 0x01;                          //Start Timer
+	while(T0TCR);                          //Wait until timer reset (1 sec delay generated)
+	T0TCR = 0x02;                          //Stop Timer
 }
 
+/***************************************************************************
+*  Funtion Name: timer_init                                                *
+***************************************************************************/
 void timer_init(void)
 {
-	if (!ERROR)
-	{
-		T0TCR = 0x02;				 //Reset Timer
+    if (!ERROR)
+    {
+        T0TCR = 0x02;		 //Reset Timer
 		T0CTCR = 0x00;       //Set Timer 0 into Timer Mode 
-	  T0MCR = 0x06;				 //Tell processor to reset timer after 1 sec
+   	    T0MCR = 0x06;		 //Tell processor to reset timer after 1 sec
 		T0MR0 = 1000;        //Value to make delay of 1sec 
-		
-		T1TCR = 0x02;				 //Reset Timer
-		T1CTCR = 0x00;       //Set Timer 0 into Timer Mode 
-	  T1MCR = 0x07;				 //Tell processor to reset timer after 1 sec
-		T1MR0 = 1000;        //Value to make delay of 1sec 
+	
+		T1TCR = 0x02;		 //Reset Timer
+		T1CTCR = 0x00;       //Set Timer 1 into Timer Mode 
+	    T1MCR = 0x07;		 //Tell processor to reset timer after 1 sec and enable timer 1 interrupt
+		T1MR0 = 1000;        //Value to make delay of 1sec
 
 	}
 }
 
+/***************************************************************************
+*  Funtion Name: pll_init                                                  *
+***************************************************************************/
 void pll_init(void)
 {
 	if (!ERROR)
