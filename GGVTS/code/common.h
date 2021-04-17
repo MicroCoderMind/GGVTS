d33081af263cc83dd0b58ae6e5686ae643f6d463
Rohan Arora <creativegroup8592@gmail.com>
2021-04-17 21:24:24 +0530

    Updated comments
    Comments to all the header files are updated.

--- GGVTS/code/common.h
+++ GGVTS/code/common.h
@@ -10,19 +10,19 @@
 #ifndef __COMMON__
 #define __COMMON__
 #undef DEBUG_START
-	  #include<LPC214X.h>	    /*This file contains all processor related addresses*/
-	  #include<string.h>	    /*This file contains all the string related C library functions*/
-	  #include<stdlib.h>	    /*This file contains all the standard C library functions*/
-	  #include"datatypes.h"	    /*This file contains all the typedefed data types*/
-	  #include"global_data.h"   /*This file contains declaration of all global variables*/
-	  #include"uart.h"	    /*This file contains declaration of UART related functions and variables*/
-	  #include"timer.h"	    /*This file contains declaration of TIMER related functions and variables*/
-	  #include"gsm.h"	    /*This file contains declaration of GSM related functions and variables*/
-	  #include"gps.h"	    /*This file contains declaration of GPS related functions and variables*/
-	  #include"functionality.h" /*This file contains declaration of various functionality functions and variables*/
-	  #include"response.h"	    /*This file contains declaration of functions and variables used to respond to user*/
-	  #include"debug.h"	    /*This file contains declaration of functions used to debug the program*/
-		#include"message_handler.h"
+	  #include<LPC214X.h>	        /* This file contains all processor related addresses */
+	  #include<string.h>	        /* This file contains all the string related C library functions */
+	  #include<stdlib.h>	        /* This file contains all the standard C library functions */
+	  #include"datatypes.h"	        /* This file contains all the typedefed data types */
+	  #include"global_data.h"       /* This file contains declaration of all global variables */
+	  #include"uart.h"	            /* This file contains declaration of UART related functions and variables */
+	  #include"timer.h"	            /* This file contains declaration of TIMER related functions and variables */
+	  #include"gsm.h"	            /* This file contains declaration of GSM related functions and variables */
+	  #include"gps.h"	            /* This file contains declaration of GPS related functions and variables */
+	  #include"functionality.h"     /* This file contains declaration of various functionality functions and variables */
+	  #include"response.h"	        /* This file contains declaration of functions and variables used to respond to user */
+	  #include"debug.h"	            /* This file contains declaration of functions used to debug the program */
+	  #include"message_handler.h"   /* This file contains declaration of functions used to authenticate user information */
 
 #endif
 
